ad16b0f3c6b4ec209badf080a95ab5d8e63a6f4c
Allendale Nato <natoallendale@gmail.com>
2016-11-08 02:48:44 +0800

    Add bower components

--- app/main.js
+++ app/main.js
@@ -8,15 +8,19 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
+  win = new BrowserWindow({width: 800, height: 600, show: false})
 
   // and load the index.html of the app.
   win.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
+    pathname: path.join(__dirname, 'views/index.html'),
     protocol: 'file:',
     slashes: true
   }))
 
+  // Only show native window if elements are ready.
+  win.on('ready-to-show', function(){
+    win.show();
+  })
 
   // Emitted when the window is closed.
   win.on('closed', () => {
