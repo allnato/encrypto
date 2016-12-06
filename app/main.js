517c2c7ae56136beca7de946f732b61cf1a635d1
Allendale Nato <natoallendale@gmail.com>
2016-12-06 15:58:15 +0800

    Add  input value in dictionary

--- app/main.js
+++ app/main.js
@@ -8,7 +8,7 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600, show: false})
+  win = new BrowserWindow({width: 1024, height: 768, show: false})
 
   // and load the index.html of the app.
   win.loadURL(url.format({
