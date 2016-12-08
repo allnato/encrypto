b96cbda5ab4d9e73dd01c1cbfe4ad614796151d5
Allendale Nato <natoallendale@gmail.com>
2016-12-08 17:34:54 +0800

    Add dictionary shift functionality

--- app/honey.js
+++ app/honey.js
@@ -5,10 +5,15 @@
 //
 //
 
-const dictionary = new Array(0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q',
+var arrayRotation = require('npm-array-rotation');
+
+const dictionary = new Array('0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q',
 'R','S','T','U','V','W','X','Y','Z');
 
-var userDict = new Array(0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q',
+var userDict = new Array('0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q',
+'R','S','T','U','V','W','X','Y','Z');
+
+var shiftDict = new Array('0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q',
 'R','S','T','U','V','W','X','Y','Z');
 
 // Edit Button is clicked
@@ -39,6 +44,11 @@ $('#honeyApply').click(function(event) {
 
 });
 
+// Encrypt Button is clicked
+$('#honeyEncrypt').click(function(event) {
+  honeyEncrypt();
+});
+
 function checkDictIfEmpty(){
   var isEmpty = true;
   $('.letter-container input').each(function(i, obj){
@@ -48,4 +58,44 @@ function checkDictIfEmpty(){
   });
 
   return isEmpty;
+}
+
+function honeyEncrypt(){
+  var encryptMessage = $('#eHoneyText').val();
+  // Shift user dictionary
+  shiftDict = arrayRotation.rotate(dictionary, parseInt($('#honeyShift').val()));
+  // Check if dictionary is enabled
+  if($('#dictToggle').prop('checked')){
+    shiftDict = arrayRotation.rotate(userDict, parseInt($('#honeyShift').val()));
+  }
+  encryptMessage = buildDictionary(encryptMessage)
+  console.log(encryptMessage);
+}
+
+function buildDictionary(message){
+  var buffer = "";
+  console.log(message.length);
+  for(i = 0; i < message.length; i++){
+    // Check if alphanumeric
+    if(/\w/.test(message[i])){
+      // Check if numeric
+      if(/[0-9]/.test(message[i])){
+        buffer += shiftDict[message[i].charCodeAt(0) - 48];
+      }
+      // Char is an alphabet
+      else {
+        // Char is in lowercase
+        if(/[a-z]/.test(message[i])){
+          buffer += shiftDict[message[i].toUpperCase().charCodeAt(0) - 55].toLowerCase();
+        } else {
+          buffer += shiftDict[message[i].toUpperCase().charCodeAt(0) - 55].toUpperCase();
+        }
+      }
+    }else {
+      buffer += message[i];
+    }
+
+  }
+
+  return buffer;
 }
